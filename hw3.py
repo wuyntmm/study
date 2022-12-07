261aaf2d8b9aeebcf1e5a253fddb4c86809b2c9a
Nazar <dgbklds@gmail.com>
2022-12-07 21:33:52 +0200

    hw3 update v2

--- hw3.py
+++ hw3.py
@@ -1,22 +1,23 @@
-print('I love Python '*42)
+print('I love Python ' * 42)
 
 age_in_month = 327
 age_in_years = age_in_month // 12
 print(age_in_years)
 
-name=git 'Nazar'
-my_age="My name is " + name + " I'm " + str(age_in_years) + " years old"
+name = git
+'Nazar'
+my_age = "My name is " + name + " I'm " + str(age_in_years) + " years old"
 print(my_age)
 
-var=1
-print(var==0)
-print(var!=5)
-print(var>3)
-print(var<17)
-print(var<=21)
+var = 1
+print(var == 0)
+print(var != 5)
+print(var > 3)
+print(var < 17)
+print(var <= 21)
 
-a=2
-b=5
-c=6
-d=str(a)+str(b)+str(c)
+a = 2
+b = 5
+c = 6
+d = str(a) + str(b) + str(c)
 print(d)
