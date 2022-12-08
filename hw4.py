c6b3abc7e05f5ca63df8f127538cabf29af117a8
Nazar <dgbklds@gmail.com>
2022-12-08 19:58:15 +0200

    hw4 updated

--- hw4.py
+++ hw4.py
@@ -1,13 +1,12 @@
 text = input('Enter the line ')
 if len(text) != 0:
     if text.isdigit():
-        print('You have entered a number and ', end='')
+        print('You have entered a number and your number is', end=' ')
         if int(text) % 2 == 0:
-            print('your number is even')
+            print('even')
         else:
-            print('your number is odd')
+            print('odd')
     else:
-        length = len(text)
-        print(f'You entered a text with a length of {length} characters')
+        print(f'You entered a text with a length of {len(text)} characters')
 else:
     print('You didn\'t entered any line')
