b1fe02da86145854d1828b75520e3cbf06b2a091
Nazar <dgbklds@gmail.com>
2023-01-13 10:54:46 +0200

    hw12 done

---
+++ hw12/hw12_1.py
@@ -0,0 +1,86 @@
+import json
+
+try:
+    with open('json') as file:
+        print('Phonebook successful loaded')
+except FileNotFoundError:
+    phonebook = {
+        'Fireman': '101',
+        'Policeman': '102',
+        'Doctor': '103'
+    }
+    print('There are no phonebook yet, let\'s create the new one')
+    with open('json', 'x+') as file:
+        file.write(json.dumps(phonebook))
+
+with open('json', 'r+') as file:
+    my_dict = json.loads(file.read())
+
+    def stats():
+        print(len(my_dict))
+
+
+    def add(key, value):
+        if my_dict.get(key) is None:
+            my_dict[key] = value
+            print(f'{key} was successful added to the dictionary')
+        else:
+            print('You are not allowed to edit')
+
+
+    def delete(key):
+        if my_dict.get(key) is not None:
+            del my_dict[key]
+            print(f'{key} successfully deleted')
+        else:
+            print(f'There are no {key} in this dictionary')
+
+
+    def names():
+        for item in my_dict.keys():
+            print(item)
+
+
+    def show(key):
+        if my_dict.get(key) is not None:
+            print(my_dict.get(key))
+        else:
+            print(f'There are no {key} in this dictionary')
+
+
+    while True:
+
+        users_input = input('What do you want to do? ')
+
+        match users_input.split()[0]:
+            case 'stats':
+                stats()
+            case 'add':
+                if len(users_input.split()) > 2:
+                    add(users_input.split()[1], users_input.split()[2])
+                else:
+                    print('Please enter the line in format \'add Name Number\'')
+            case 'delete':
+                if len(users_input.split()) > 1:
+                    delete(users_input.split()[1])
+                else:
+                    print('Please enter the line in format \'delete Name\'')
+            case 'list':
+                names()
+            case 'show':
+                if len(users_input.split()) == 2:
+                    show(users_input.split()[1])
+                elif len(users_input.split()) > 2:
+                    print('There are no', end=' ')
+                    for item in users_input.split():
+                        print(item, end=' ')
+                    print('in this dictionary')
+                else:
+                    print('Please enter the line in format \'show Name\'')
+            case 'exit':
+                break
+            case _:
+                print('I don\'t understand you. Please use commands: stats, add, delete, list, show or exit')
+    file.truncate(0)
+    file.seek(0)
+    file.write(json.dumps(my_dict))
---
+++ hw12/hw12_2-3.py
@@ -0,0 +1,29 @@
+import time
+
+
+def name_and_time(func):
+
+    def inner_deco_func(*args, **kwargs):
+        with open('call_log.txt', 'a') as file:
+            print(f'You called function "{func.__name__}" on {time.ctime()}', file=file)
+        return func(*args, **kwargs)
+
+    return inner_deco_func
+
+
+@name_and_time
+def test():
+    print('Test')
+    raise MyCustomException('Houston, we have a problem')
+
+
+class MyCustomException(Exception):
+    def __init__(self, message):
+        with open('exception_log.txt', 'a') as file:
+            print(f'{message} on {time.ctime()}', file=file)
+
+
+try:
+    test()
+except MyCustomException as ex:
+    print(f'There was custom exception: {ex}')
