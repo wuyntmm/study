656d0cc69e0087a6243c6ddf09e4c93250a029ac
Nazar <dgbklds@gmail.com>
2023-01-21 11:23:55 +0200

    upd

--- hw14_1.py
+++ hw14_1.py
@@ -5,7 +5,7 @@ user_input = input('Enter file name in format File.txt \n')
 try:
     with open(f'{user_input}', 'r+') as file:
         text = file.read()
-        text = re.sub(r"(?P<first>\w)\w{2,}@\w+\.\w+(?=\w)(?P<last>\w)", r"\g<first>***@***\g<last>", text)
+        text = re.sub(r"(?P<first>\w)[\w.]{2,}@\w+\.\w+(?=\w)(?P<last>\w)", r"\g<first>***@***\g<last>", text)
 
         file.truncate(0)
         file.seek(0)
