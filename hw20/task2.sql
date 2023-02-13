a2c42c8b3f30d3df77c135e4a125018bda541549
Nazar <dgbklds@gmail.com>
2023-02-13 16:15:38 +0200

    hw20

---
+++ hw20/task2.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (first_name, last_name, age)
+VALUES
+    ('John', 'Johnson', 21),
+    ('Tom', 'Joohn', 22),
+    ('Mike', 'Josh', 23),
+    ('Phil', 'Joh', 24),
+    ('Dave', 'Jons', 25)
