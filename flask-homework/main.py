f6d6e3984b474a64ee8682f70a15a86701b005cb
Nazar <dgbklds@gmail.com>
2023-04-02 19:44:03 +0300

    upd

--- flask-homework/main.py
+++ flask-homework/main.py
@@ -1,1 +1,25 @@
-# test
+from flask import Flask
+from logging.config import dictConfig
+
+app = Flask(__name__)
+
+dictConfig({
+    "version": 1,
+    "formatters": {
+        "simple": {"format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s"}
+    },
+    "handlers": {
+        "console": {"class": "logging.StreamHandler", "formatter": "simple"}
+    },
+    "root": {"handlers": ["console"], "level": "INFO"},
+})
+
+
+@app.route('/hello')
+def index():
+    app.logger.info('INFO was logged successfully')
+    return 'Hello, world!'
+
+
+if __name__ == '__main__':
+    app.run()
