5a8994e35e71f11b7f19f3ac8298961561c40bc1
Nazar <dgbklds@gmail.com>
2023-04-03 22:23:34 +0300

    hw34

--- flask-homework/homea_pp/views.py
+++ flask-homework/homea_pp/views.py
@@ -1,8 +1,8 @@
 from .__init__ import app
 import random
-from flask import request, redirect
+from flask import request, redirect, render_template, session, url_for
 
-
+app.secret_key = b'1234'
 @app.route('/hello')
 def index():
     app.logger.info('INFO was logged successfully')
@@ -11,79 +11,82 @@ def index():
 
 @app.get('/users')
 def get_users():
-    names = ["Emily", "Jacob", "Ava", "Mia", "Noah", "Ethan", "Isabella", "Oliver", "Sophia", "Charlotte"]
-    res = '<ul>'
-    for i in range(random.randint(2, 10)):
-        res += f'<li>{random.choice(names)}</li>'
-    res += '</ul>'
-    return res, 200
+    user = session.get('user')
+    if user:
+        names = ["Emily", "Jacob", "Ava", "Mia", "Noah", "Ethan", "Isabella", "Oliver", "Sophia", "Charlotte"]
+        random_names = []
+        for i in range(random.randint(2, 10)):
+            random_names.append(random.choice(names))
+        return render_template('users.html', random_names=random_names, user=user), 200
+    else:
+        return redirect('/login')
 
 
 @app.get('/books')
 def get_book():
-    books = [
-        "To Kill a Mockingbird by Harper Lee",
-        "1984 by George Orwell",
-        "The Catcher in the Rye by J.D. Salinger",
-        "The Great Gatsby by F. Scott Fitzgerald",
-        "Pride and Prejudice by Jane Austen",
-        "One Hundred Years of Solitude by Gabriel Garcia Marquez",
-        "The Hitchhiker's Guide to the Galaxy by Douglas Adams",
-        "The Hobbit by J.R.R. Tolkien",
-        "The Lord of the Rings by J.R.R. Tolkien",
-        "The Da Vinci Code by Dan Brown"
-    ]
-    res = '<ul>'
-    for i in range(random.randint(2, 10)):
-        res += f'<li>{random.choice(books)}</li>'
-    res += '</ul>'
-    return res, 200
-
+    user = session.get('user')
+    if user:
+        books = [
+            "To Kill a Mockingbird by Harper Lee",
+            "1984 by George Orwell",
+            "The Catcher in the Rye by J.D. Salinger",
+            "The Great Gatsby by F. Scott Fitzgerald",
+            "Pride and Prejudice by Jane Austen",
+            "One Hundred Years of Solitude by Gabriel Garcia Marquez",
+            "The Hitchhiker's Guide to the Galaxy by Douglas Adams",
+            "The Hobbit by J.R.R. Tolkien",
+            "The Lord of the Rings by J.R.R. Tolkien",
+            "The Da Vinci Code by Dan Brown"
+        ]
+        books_list = []
+        for i in range(random.randint(2, 10)):
+            books_list.append(random.choice(books))
+        return render_template('books.html', books_list=books_list, user=user), 200
+    else:
+        return redirect('/login')
 
 @app.get('/users/<int:users_id>')
 def get_users_by_id(users_id):
-    if users_id % 2:
-        return '<h1>Not Found</h1>', 404
+    user = session.get('user')
+    if user:
+        if users_id % 2:
+            return render_template('user_not_found.html', user=user), 404
+        else:
+            return render_template('user_id.html', users_id=users_id, user=user)
     else:
-        return f'<div>Your ID: {users_id}', 200
-
+        return redirect('/login')
 
 @app.get('/books/<string:title>')
 def get_book_title(title):
-    return f'<h1>{str(title).title()}</h1>', 200
-
+    user = session.get('user')
+    if user:
+        return render_template('books_title.html', title=str(title).title(), user=user), 200
+    else:
+        return redirect('/login')
 
 @app.route('/params')
 def get_params():
-    params = request.args.to_dict()
-    table = '<table><tr><th>parameter</th><th>value</th></tr>'
-    for key, value in params.items():
-        table += f'<tr><td>{key}</td><td>{value}</td></tr>'
-    table += '</table>'
-    return table, 200
-
+    user = session.get('user')
+    if user:
+        params = request.args.to_dict()
+        return render_template('params.html', params=params, user=user)
+    else:
+        return redirect('/login')
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
     if request.method == "GET":
-        form = '''
-        <form method="post" action="/login">
-            <label>Username:</label>
-            <input type="text" name="username"><br><br>
-            <label>Password:</label>
-            <input type="password" name="password"><br><br>
-            <button type="submit">Send form</button>
-        </form>
-        '''
-        return form, 200
+        return render_template('login.html'), 200
 
     elif request.method == "POST":
         username = request.form.get('username')
         password = request.form.get('password')
         if username and password:
+            session['user'] = username
+            session['password'] = password
             return redirect('/users')
         else:
-            return '<h1>Data is missing</h1>', 400
+            return render_template('login_fail.html'), 400
 
 
 @app.errorhandler(404)
@@ -96,3 +99,8 @@ def page_not_found(e):
 def internal_server_error(e):
     app.logger.info(f'{e.name} occurs')
     return '<h1>Exception 500, try again</h1>', 500
+
+@app.post('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
