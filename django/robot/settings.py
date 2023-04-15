1aa55bf02e39cee63cda47f6b6fae6036132a60a
Nazar <dgbklds@gmail.com>
2023-04-15 11:46:04 +0300

    fix secret key

--- django/robot/settings.py
+++ django/robot/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@_wj(0vij8sw^0_lcnw=ia$))i=n2=^*r%#imyb45((y01^#b%'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
