0b807ac934b06d27718f81501a169aa10160e29b
Nazar <dgbklds@gmail.com>
2023-05-09 00:01:47 +0300

    done

--- django/user/urls.py
+++ django/user/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import ListUser, DetailUser, CreateUser, APIUser
+from .views import *
+from rest_framework.routers import SimpleRouter
 
 urlpatterns = [
-    path('', ListUser.as_view(), name='user-list'),
+    path('list', ListUser.as_view(), name='user-list'),
     path('<int:pk>', DetailUser.as_view(), name='user-detail'),
     path('add/', CreateUser.as_view(), name='user-create'),
     path('apiuser', APIUser.as_view(), name='api-user'),
 ]
+
+router = SimpleRouter()
+router.register('', UserViewSet)
+
+urlpatterns += router.urls
