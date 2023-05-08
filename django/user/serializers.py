59e5f9b616ece6fe597fa4983c48ed7f81f3744a
Nazar <dgbklds@gmail.com>
2023-05-09 00:01:47 +0300

    done

--- django/user/serializers.py
+++ django/user/serializers.py
@@ -5,4 +5,4 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'age', )
+        fields = '__all__'
